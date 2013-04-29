4191ee68023514e4155d6852b9cbddaccaac3c70
Fatih GÜRSOY <fatihgursoy@ymail.com>
2013-04-29 09:54:59 +0300

    Indentation Fix

--- src/Runners/Yaml.php
+++ src/Runners/Yaml.php
@@ -6,21 +6,19 @@ use Muhafiz\Runners\RunnersAbstract as RunnersAbstract;
 
 class Yaml extends RunnersAbstract{
 	protected $_name = 'Yaml';
-    protected $_toolName = "yaml";
-    protected $_toolCheckCommand = "php -i | grep yaml";
-    protected $_fileFilterRegexp = "/.*\.(yml|yaml)$/";
+	protected $_toolName = "yaml";
+	protected $_toolCheckCommand = "php -i | grep yaml";
+	protected $_fileFilterRegexp = "/.*\.(yml|yaml)$/";
 
-    function run(array $files)
-    {
-        foreach ($files as $file) {
-			$contents = fread(fopen($file, "r"), filesize($file)); 
-			$parse = yaml_parse($contents); 
-			
+	function run(array $files) {
+		foreach ($files as $file) {
+			$contents = fread(fopen($file, "r"), filesize($file));
+			$parse = yaml_parse($contents);
 			if (!is_array($parse)) {
-                $out['output'][] = "'${file}' is not valid.";
-                $this->_onRuleFailed($out);
-            }
-        }
-    }
+				$out['output'][] = "'${file}' is not valid.";
+				$this->_onRuleFailed($out);
+ 			}
+		}
+	}
 }
 ?>
