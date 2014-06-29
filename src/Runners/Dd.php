fd7d133f3f3ac8f3d6ed01d065ab94761fdb0f75
Emir Karşıyakalı <emirkarsiyakali@gmail.com>
2014-06-30 01:19:32 +0300

    added laravel specific dd(die and dump) statement checker

---
+++ src/Runners/Dd.php
@@ -0,0 +1,47 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+namespace Muhafiz\Runners;
+use Muhafiz\Utils\System as Sys;
+use Muhafiz\Runners\RunnersAbstract as RunnersAbstract;
+
+/**
+ * Check if php files contains laravel specific dd statement
+ */
+class Dd extends RunnersAbstract
+{
+    protected $_name = "dd";
+    protected $_toolName = "grep";
+    protected $_toolCheckCommand = "which grep";
+    protected $_fileFilterRegexp = "/\.ph(p|tml)$/"; //php and phtml files should be checked
+
+    function run(array $files)
+    {
+        foreach ($files as $file) {
+            //check if files have dd statement
+            //but dont check files which are commented with //
+            $out = Sys::runCommand(
+                $this->_vcs->catCommand($file) .
+                " | grep -vE '^\s*\/\/' | grep -iqE 'dd' 2>&1"
+            );
+
+            if ($out['exitCode'] == 0) {
+                $out['output'][] = "'${file}' file contains dd statement";
+                $this->_onRuleFailed($out);
+            }
+        }
+    }
+}
